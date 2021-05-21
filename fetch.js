9a95b29bb6d16a571a34593b2a3ca0df26c4a3c4
Letticia Nicoli <letticia.nicoli@nubank.com.br>
2021-05-21 14:24:39 -0300

    Refactoring fetch csv

--- fetch.js
+++ fetch.js
@@ -1,20 +1,4 @@
-import { readCSV, writeCSV } from '../../src/csv.ts'
+import { readCSV, writeJSON } from 'https://deno.land/x/flat/mod.ts'
 const csv = await readCSV('./release.csv')
-
-// const newdata = [] 
-// const rows = data.feed.entry
-
-// for(const row of rows) {
-//     const formattedRow = {}
-
-//     for(const key in row) {
-//       if(key.startsWith("gs$")) {
-//         formattedRow[key.replace("gs$", "")] = row[key].$t
-//       }
-//     }
-
-//     newdata.push(formattedRow)
-//   }
-
 const newfile = `subset_of_csv.json`
 await writeJSON(newfile, csv)
